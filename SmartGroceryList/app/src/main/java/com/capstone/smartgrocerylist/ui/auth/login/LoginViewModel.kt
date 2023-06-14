6cfbeb68a7c3e59c3c179e40bd01216b043cd3b7
Aldiansyah Pramudia Hasibuan <63297603+aldiansyahpramudia@users.noreply.github.com>
2023-06-14 11:11:58 +0700

    Delete SmartGroceryList directory

--- SmartGroceryList/app/src/main/java/com/capstone/smartgrocerylist/ui/auth/login/LoginViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.capstone.smartgrocerylist.ui.auth.login
-
-import androidx.lifecycle.ViewModel
-import com.capstone.smartgrocerylist.data.GroceryRepository
-
-class LoginViewModel(private val storyRepository: GroceryRepository) : ViewModel() {
-    fun login(email: String, password: String) =
-        storyRepository.postLogin(email, password)
-}
